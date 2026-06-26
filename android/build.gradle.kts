a25f5d384861109e4781ed20b6d9683420b72c78
GoldenTitab <suicidewithmeagain@gmail.com>
2026-06-26 14:01:39 +0330

    initial commit

---
+++ android/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("com.android.application") apply false
+    id("com.android.library") apply false
+    id("org.jetbrains.kotlin.android") apply false
+}
+
+allprojects {
+    repositories {
+        maven { url = uri("https://storage.flutter-io.cn/download.flutter.io") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        google()
+        mavenCentral()
+    }
+}
+
+val newBuildDir: Directory =
+    rootProject.layout.buildDirectory
+        .dir("../../build")
+        .get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+subprojects {
+    if (project.name != "app") {
+        project.evaluationDependsOn(":app")
+    }
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
---
+++ lib/views/login_view.dart
@@ -0,0 +1,77 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('صفحه ورود'),
+        actions: [IconButton(icon: Icon(Icons.search), onPressed: () {})],
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _email,
+            keyboardType: TextInputType.emailAddress,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: InputDecoration(hintText: "ایمیل خود را وارد کنید."),
+          ),
+          TextField(
+            controller: _password,
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: InputDecoration(hintText: "رمز خود را وارد کنید."),
+          ),
+          TextButton(
+            onPressed: () async {
+              try {
+                final userCredential = await FirebaseAuth.instance
+                    .signInWithEmailAndPassword(
+                      email: _email.text,
+                      password: _password.text,
+                    );
+                // final userCredential = await FirebaseAuth.instance
+                //     .createUserWithEmailAndPassword(
+                //       email: _email.text,
+                //       password: _password.text,
+                //     );
+                print(userCredential);
+              } on FirebaseAuthException catch (e) {
+                print('CODE: ${e.code}');
+                print('MESSAGE: ${e.message}');
+              }
+            },
+            child: Text("ورود"),
+          ),
+        ],
+      ),
+    );
+  }
+}
