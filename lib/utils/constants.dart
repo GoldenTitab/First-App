6b41d7956bb82bae0cafe3388dd827ebe2ab5fa1
GoldenTitab <suicidewithmeagain@gmail.com>
2026-07-01 14:43:23 +0330

    constant texts edited

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -5,24 +5,24 @@ class AppRoutes {
 
 class AppStrings {
   static const String appTitle = 'Aurora';
-  static const String loginTitle = 'صفحه ورود';
-  static const String registerTitle = 'صفحه ثبت‌نام';
+  static const String loginTitle = 'ورود';
+  static const String registerTitle = 'ثبت‌نام';
   static const String verifyEmailTitle = 'تأیید ایمیل';
   static const String homeTitle = 'خانه';
   static const String emailHint = 'ایمیل خود را وارد کنید.';
   static const String passwordHint = 'رمز خود را وارد کنید.';
   static const String loginButton = 'ورود';
   static const String registerButton = 'ثبت‌نام';
-  static const String goToRegister = 'ثبت‌نام نکرده‌اید؟ ایجاد حساب جدید';
+  static const String goToRegister = 'ثبت‌نام نکرده‌اید؟ ایجاد حساب جدید...';
   static const String logoutDialogTitle = 'خروج از حساب';
   static const String logoutDialogContent =
       'آیا مطمئن هستید که می‌خواهید خارج شوید؟';
   static const String cancel = 'لغو';
   static const String logout = 'خروج';
-  static const String welcome = 'به صفحه اصلی خوش آمدید';
+  static const String welcome = 'به صفحه اصلی خوش آمدید.';
   static const String verifyEmailMessage = 'لطفاً ایمیل خود را تأیید کنید.';
   static const String emailVerificationSent =
-      'ایمیل تأیید ارسال شد. لطفاً صندوق ورودی خود را چک کنید.';
+      'ایمیل تأیید ارسال شد.\nلطفاً صندوق ورودی خود را چک کنید.';
   static const String genericError = 'خطایی رخ داده است.';
   static const String userNotFound = 'کاربری با این ایمیل یافت نشد.';
   static const String wrongPassword = 'رمز عبور اشتباه است.';
