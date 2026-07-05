2730ac7d05683cfee2c6a3870d0fad6dd39e7578
GoldenTitab <suicidewithmeagain@gmail.com>
2026-07-05 15:52:05 +0330

    deleted an unused import

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import '../services/auth_service.dart';
 import '../utils/constants.dart';
-import '../utils/app_theme.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
