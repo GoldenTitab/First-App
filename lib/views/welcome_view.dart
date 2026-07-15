b5df332119ede4ba6f3fec60d1b2637313674583
GoldenTitab <suicidewithmeagain@gmail.com>
2026-07-15 11:34:18 +0330

    added a new page called welcome_view.dart

--- lib/views/welcome_view.dart
+++ lib/views/welcome_view.dart
@@ -1,1 +1,25 @@
 import 'package:flutter/material.dart';
+
+class WelcomeView extends StatefulWidget {
+  const WelcomeView({super.key});
+
+  @override
+  State<WelcomeView> createState() => _WelcomeViewState();
+}
+
+class _WelcomeViewState extends State<WelcomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            width: 200,
+            height: 300,
+            child: Image.asset('assets/images/download.png', fit: BoxFit.cover),
+          ),
+        ],
+      ),
+    );
+  }
+}
